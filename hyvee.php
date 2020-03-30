c127ed45eccce7ac8f1ad70af2d1b081c7848137
John Hass <john@Johns-MacBook-Pro.local>
2020-03-30 16:09:02 -0500

    Initial Cart Support

--- hyvee.php
+++ hyvee.php
@@ -13,6 +13,16 @@ curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
 $result = curl_exec($ch);
 
+$ch =  curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/account/login.aspx?retUrl=%2F" );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
+$result = curl_exec($ch);
+
 curl_setopt($ch, CURLOPT_URL, "https://hy-vee.com/account/login.aspx" );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
 curl_setopt($ch, CURLOPT_AUTOREFERER, true );
@@ -34,7 +44,7 @@ foreach($lines as $line) {
 }
 if ($post_url == "") {
     if (!strstr($result,"Order History")) {
-        echo "No Soup For You -- No Post URL For Login";
+        echo "No Soup For You -- No Post URL For Login\n";
         exit;
     }
 }
@@ -54,16 +64,81 @@ if ($post_url != "") {
 }
 
 if (!strstr($result,"Order History") && !strstr($result,"My Recipes")) {
-    echo "Error Logging In";
+    echo "Error Logging In\n";
     exit;
 }
 
-echo "Let's Go Shopping!\n";
+curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com" );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
+$result = curl_exec($ch);
+
+curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/aisles-online/api/aisles-online-api" );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+curl_setopt($ch, CURLOPT_REFERER, 'https://www.hy-vee.com/aisles-online/checkout/cart?iframe=true');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*','Accept-Encoding: gzip, deflate, br','Accept-Language: en-US,en;q=0.5','Cache-Control: max-age=0','Connection: keep-alive','content-type: application/json','Origin: https://www.hy-vee.com','TE: Trailers'));
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
+curl_setopt($ch, CURLOPT_POSTFIELDS,'[{"operationName":"getHomeBanners","variables":{"authType":"TWO_LEGGED_AUTH_TYPE"},"query":"query getHomeBanners {\n  banners(where: {isActive: true}) {\n    ...bannerFragment\n    subtitle\n    description\n    productGroupId\n    __typename\n  }\n}\n\nfragment bannerFragment on banner {\n  bannerId\n  mainHref\n  imageUri\n  isActive\n  type\n  title\n  __typename\n}\n"}]');
+$result = curl_exec($ch);
+
+
+curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/aisles-online/login?redirect=%2Faisles-online%2Fiframe-modal" );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+curl_setopt($ch, CURLOPT_POST, 0);
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+curl_setopt($ch, CURLOPT_REFERER, 'https://www.hy-vee.com/grocery/');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Language: en-US,en;q=0.5','Cache-Control: max-age=0','Connection: keep-alive','Origin: https://www.hy-vee.com','TE: Trailers','Upgrade-Insecure-Requests: 1'));
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
+$result = curl_exec($ch);
+//echo $result;
+
+curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/my-account" );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+curl_setopt($ch, CURLOPT_POST, 0);
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+curl_setopt($ch, CURLOPT_REFERER, 'https://www.hy-vee.com/grocery/');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Language: en-US,en;q=0.5','Cache-Control: max-age=0','Connection: keep-alive','Origin: https://www.hy-vee.com','TE: Trailers','Upgrade-Insecure-Requests: 1'));
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
+$result = curl_exec($ch);
+$doc = new DOMDocument();
+@$doc->loadHTML($result);
+$xpath = new DOMXPath($doc);
+$items = $xpath->query("//script[@id='__NEXT_DATA__']");
+$json = json_decode($items[0]->textContent);
+
+$firstName = $json->props->initialReduxState->user->customer->firstName;
+$lastName = $json->props->initialReduxState->user->customer->lastName;
+$customerId = $json->props->initialReduxState->user->customer->customerId;
+$customerUuid= $json->props->initialReduxState->user->customer->customerUuid;
+$username = $json->props->initialReduxState->user->customer->username;
+$preferredStoreId = $json->props->initialReduxState->user->customer->preferredStoreId;
+$fuelSaverCardUuid= $json->props->initialReduxState->user->customer->fuelSaverCardUuid;
+
+curl_close($ch);
+$ch = curl_init();
+
+
+echo "Welcome $firstName $lastName Let's Go Shopping!\n";
 
 while (42 == 42) {
     $command = readline("> ");
     if ($command == "?") {
-        echo "Type Item To Search";
+        echo "Type item To Search";
     } else if ($command == "quit") {
         exit;
     } else if (startsWith($command, "add ")) {
@@ -105,6 +180,7 @@ while (42 == 42) {
         curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/grocery/search?search=".$item );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
         curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+	curl_setopt($ch, CURLOPT_POST, 0);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
         curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
         curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
@@ -148,15 +224,38 @@ while (42 == 42) {
             echo "Added\n";
         }
     } else if ($command == "cart") {
-        curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/shop/checkout/cart.aspx");
+	curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/shop/checkout/cart.aspx" );
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt($ch, CURLOPT_AUTOREFERER, true );
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+	curl_setopt($ch, CURLOPT_POST, 0);
+	curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+	curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
+	$result = curl_exec($ch);
+
+        curl_setopt($ch, CURLOPT_URL, "https://www.hy-vee.com/aisles-online/api/aisles-online-api" );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
         curl_setopt($ch, CURLOPT_AUTOREFERER, true );
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
         curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
+	curl_setopt($ch, CURLOPT_REFERER, 'https://www.hy-vee.com/aisles-online/checkout/cart?iframe=true');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*','Accept-Encoding: gzip, deflate, br','Accept-Language: en-US,en;q=0.5','Cache-Control: max-age=0','Connection: keep-alive','content-type: application/json','Origin: https://www.hy-vee.com','TE: Trailers'));
+	curl_setopt($ch,CURLOPT_POSTFIELDS,'[{"operationName":"getActiveCart","variables":{"customerId":'.$customerId.'},"query":"query getActiveCart($customerId: Int!) {\n  customer(customerId: $customerId) {\n    customerId\n    customerMemberships {\n      customerMembershipId\n      isActive\n      membership {\n        membershipId\n        type\n        __typename\n      }\n      __typename\n    }\n    firstName\n    __typename\n  }\n  carts(customerId: $customerId, where: {isActive: true}) {\n    cartId\n    customerId\n    storeId\n    fulfillmentExpiration\n    fulfillmentId\n    fulfillmentWindowStart\n    fulfillmentWindowEnd\n    fulfillmentType\n    fulfillmentLocation {\n      fulfillmentLocationId\n      fulfillmentStoreId\n      locationName\n      fulfillmentFees {\n        feeWithMembership\n        feeWithoutMembership\n        orderMinimumAmount\n        duration\n        __typename\n      }\n      fulfillmentTimes {\n        duration\n        windowStart\n        windowEnd\n        __typename\n      }\n      __typename\n    }\n    cartItems {\n      cartItemId\n      note\n      quantity\n      quantityType\n      storeProduct {\n        storeProductId\n        onSale\n        onFuelSaver\n        isWeighted\n        isActive\n        isAlcohol\n        fuelSaver\n        price\n        priceMultiple\n        taxRate\n        department {\n          departmentId\n          __typename\n        }\n        product {\n          name\n          productId\n          size\n          averageWeight\n          upc\n          isNotEligibleForDelivery\n          productImages(where: {isPrimary: true, size: \"THUMBNAIL\"}) {\n            uri\n            size\n            isPrimary\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      tax\n      __typename\n    }\n    cartPromotions {\n      cartPromotionId\n      isApplied\n      promotion {\n        promotionId\n        promoCode\n        description\n        threshold\n        amount\n        isPercentage\n        isActive\n        startDate\n        endDate\n        allowedUsages\n        isStackable\n        isFuelSaver\n        newCustomersOnly\n        isAvailableForAllOrderTypes\n        isAvailableForAllStores\n        __typename\n      }\n      __typename\n    }\n    deliveryAddress {\n      addressOne\n      addressTwo\n      city\n      state\n      zip\n      fulfillmentLocations {\n        fulfillmentLocationId\n        __typename\n      }\n      __typename\n    }\n    pickupLocation {\n      pickupLocationId\n      name\n      address\n      city\n      state\n      zip\n      isActive\n      pickupLocationHasLocker\n      __typename\n    }\n    store {\n      name\n      address\n      city\n      state\n      zip\n      __typename\n    }\n    __typename\n  }\n}\n"}]');
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
         $result = curl_exec($ch);
-        echo "Cart is broken on the Hy-Vee Side Right now\n";
+        $j = json_decode($result);
+	$count = 0;
+	//print_r($j[0]->data->carts[0]->cartItems);
+	foreach($j[0]->data->carts as $cart) {
+		foreach($cart->cartItems as $item) {			
+			//print_r($item);
+			echo $count . " Name: " . $item->storeProduct->product->name . "\n";
+			$count++;
+		}
+	}
     } else {
         //search it https://www.hy-vee.com/grocery/search?search=
         $item = urlencode($command);
@@ -165,6 +264,7 @@ while (42 == 42) {
         curl_setopt($ch, CURLOPT_AUTOREFERER, true );
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
         curl_setopt($ch, CURLOPT_COOKIEJAR,"cookies");
+	curl_setopt($ch, CURLOPT_POST, 0);
         curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies");
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0');
         $result = curl_exec($ch);
